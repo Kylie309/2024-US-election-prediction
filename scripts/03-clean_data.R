77236c1419da5471c5a5c5b5431f1231258790d1
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-10-22 06:13:47 -0400

    Updated data cleaning script

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,33 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw poll dataset
+# Author: Yunkai Gu & Anqi Xu & Yitong Wang
+# Date: 22 October 2024
+# Contact: kylie.gu@mail.utoronto.ca & anjojoo.xu@mail.utoronto.ca & stevenn.wang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Downloaded raw dataset from https://projects.fivethirtyeight.com/polls/president-general/2024/national/
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
 
+
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/01-raw_data/poll_raw_data.csv")
 
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
+  filter(numeric_grade >= 2.9) |> 
+  # filter only the high-quality pollsters
+  mutate(hypothetical = ifelse(hypothetical == "TRUE", 1, 0)) |> 
+  # convert binary variable hypothesis into value 1 and 0
+  select(pollster, transparency_score, state, sample_size, hypothetical, 
+         candidate_name, pct) |> 
+  # select columns needed for analysis
+  filter(candidate_name == "Kamala Harris" | candidate_name == "Donald Trump" 
+         | candidate_name == "Donald Trump Jr.") |>
+  # select target candidate
   tidyr::drop_na()
 
+
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "data/02-analysis_data/poll_analysis_data.csv")
