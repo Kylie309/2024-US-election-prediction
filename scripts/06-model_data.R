6c188ca7aac86e61ce2c4c3f755bacf8fec6b056
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-10-25 08:28:34 -0400

    Uploaded script for modeling data

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Models percentage of vote with multiple variables
+# Purpose: Models percentage of vote with date and state
 # Author: Yunkai Gu & Anqi Xu & Yitong Wang
-# Date: 22 October 2024
+# Date: 25 October 2024
 # Contact: kylie.gu@mail.utoronto.ca & anjojoo.xu@mail.utoronto.ca & stevenn.wang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Cleaned the dataset
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? None
 
 
 #### Workspace setup ####
@@ -16,48 +16,63 @@ library(broom)
 library(modelsummary)
 library(rstanarm)
 library(splines)
+library(arrow)
 
 
 #### Read data ####
-analysis_data <- read_csv("data/02-analysis_data/poll_analysis_data.csv")
-just_trump_data <- 
-  analysis_data |> filter(candidate_name == "Donald Trump" | 
-                          candidate_name == "Donald Trump Jr.") |>
-  mutate(num_trump = round((pct / 100) * sample_size, 0))
+trump_national_data <- read_parquet("data/02-analysis_data/trump_national_data.parquet")
+trump_state_data <- read_parquet("data/02-analysis_data/trump_state_data.parquet")
 
 
-### Starter Models ####
+#### Bayesian models and splines ####
 
-# Model 1: pct as a function of sample size
-model_size <-
-  lm(pct ~ sample_size, data = just_trump_data)
-summary(model_size)
-
-# Model 2: pct as a function of sample size and pollster
-model_size_pollster <-
-  lm(pct ~ sample_size + pollster,
-     data = just_trump_data)
-summary(model_size_pollster)
-
-# Augment data with model predictions
-just_trump_data <- just_trump_data |>
+# Change 'pollster' and 'state' to factor variables
+trump_national_data <- trump_national_data |>
   mutate(
-    fitted_size = predict(model_size),
-    fitted_size_pollster = predict(model_size_pollster)
+    pollster = factor(pollster),
+    state = factor(state)
   )
 
-# Model summary
-modelsummary(models = list("Model 1" = model_size, 
-                           "Model 2" = model_size_pollster))
+trump_state_data <- trump_state_data |>
+  mutate(
+    pollster = factor(pollster),
+    state = factor(state)
+  )
+
+# Fit Bayesian model with spline for prediction of the whole nation
+spline_model_national <- stan_glm(
+  pct ~ ns(end_date_num, df = 5), # Change df for the number of "bits" - higher numbers - more "wiggly" - but then need to worry about overfitting.
+  data = trump_national_data,
+  family = gaussian(),
+  prior = normal(0, 5),
+  prior_intercept = normal(50, 10),
+  seed = 304,
+  iter = 2000,
+  chains = 4,
+  refresh = 0
+)
+
+# Fit Bayesian model with spline and state as fixed effect
+spline_model_state <- stan_glm(
+  pct ~ ns(end_date_num, df = 5) + state,
+  data = trump_state_data,
+  family = gaussian(),
+  prior = normal(0, 5),
+  prior_intercept = normal(50, 10),
+  seed = 304,
+  iter = 2000,
+  chains = 4,
+  refresh = 0
+)
 
 
 #### Save model ####
 saveRDS(
-  model_size,
-  file = "models/trump_model_size.rds"
+  spline_model_national,
+  file = "models/model_national.rds"
 )
 
 saveRDS(
-  model_size_pollster,
-  file = "models/trump_model_size_pollster.rds"
+  spline_model_state,
+  file = "models/model_state.rds"
 )
