ec96e169119534cdebfb86841923ebafcfb45f97
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-10-25 08:27:09 -0400

    Uploaded script for data download process

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,22 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from fivethirtyeight (https://projects.fivethirtyeight.com/polls/president-general/2024/national/)
+# Author: Yunkai Gu & Anqi Xu & Yitong Wang
+# Date: October 25 2024
+# Contact: kylie.gu@mail.utoronto.ca & anjojoo.xu@mail.utoronto.ca & stevenn.wang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+raw_data <- read_csv("https://projects.fivethirtyeight.com/polls/data/president_polls.csv")
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_data, "data/01-raw_data/poll_raw_data.csv") 
 
          
